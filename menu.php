8708a5f20eb76b6afd076e7fe77ae424727a6a77
irailson <ifateicha@dcc.ufba.br>
2014-03-20 12:52:45 -0300

    parte do views do menu

---
+++ menu.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8" />
+		<title>menu</title>
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+	</head>
+	<body>
+	
+		<div class="container-menu">
+			<!-- Top Navigation -->	
+			<img src="images/InfoJr - Preta (1).png">
+			<section>
+				<nav class="nav-menu color-7 cl-effect-4">
+					<a href="#">Home</a>
+					<a href="#">Empresa</a>
+					<a href="#">Portifólio</a>
+					<a href="#">Notícias</a>
+					<a href="#">Contato</a>
+				</nav>
+			</section>
+		</div><!-- /container -->
+		1
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>sadfsdfsadfsad
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				1asdfsadf-->
+	</body>
+</html>
